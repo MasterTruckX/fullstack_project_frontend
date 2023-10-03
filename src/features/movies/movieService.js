b1fd48b12d527136f01c5ead370b65a2ee333ecd
unknown <jorge.orozco@cetys.edu.mx>
2023-10-03 16:34:17 -0700

    Movie service and slice features created

---
+++ src/features/movies/movieService.js
@@ -0,0 +1,46 @@
+import axios from 'axios'
+
+const API_URL = 'https://tiny-ruby-moth-tux.cyclic.cloud/ouraMovies/movies/'
+
+// //crear una nueva tarea
+// const createTarea = async (tareaData, token) => {
+//     const config = {
+//         headers: {
+//             Authorization: `Bearer ${token}`
+//         }
+//     }
+//     const response = await axios.post(API_URL,tareaData, config)
+
+//     return response.data
+// }
+
+//obtener tareas
+const getMovie = async (token) => {
+    const config = {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }
+    const response = await axios.get(API_URL, config)
+
+    return response.data
+}
+
+//borrar tareas
+const deleteMovie = async (token) => {
+    const config = {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }
+    const response = await axios.delete(API_URL+id, config)
+
+    return response.data
+}
+
+const tareaService = {
+    getMovie,
+    deleteMovie
+}
+
+export default movieService
---
+++ src/features/movies/movieSlice.js
@@ -0,0 +1,48 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import movieService from './movieService'
+
+const initialState = {
+    movies: [],
+    isError: false,
+    isSuccess: false,
+    isLoading: false,
+    message: ''
+}
+
+// get movies
+export const getMovie = createAsyncThunk('movies/get', async (_, thunkAPI) =>{
+    try{
+        const token = thunkAPI.getState().auth.user.token
+        return await movieService.getMovie(token)
+    } catch (error) {
+        const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
+        return thunkAPI.rejectWithValue(message)
+    }
+})
+
+export const movieSlice = createSlice({
+    name: 'movie',
+    initialState,
+    reducers: {
+        reset: (state) => initialState
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(getMovie.pending, (state) => {
+                state.isLoading = true
+            })
+            .addCase(getMovie.fulfilled, (state, action) => {
+                state.isLoading = false
+                state.isSuccess = true
+                state.movies = action.payload
+            })
+            .addCase(getMovie.rejected, (state, action) => {
+                state.isLoading = false
+                state.isError = true
+                state.message = action.payload
+            })
+    }
+})
+
+export const {reset} = movieSlice.actions
+export default movieSlice.reducer
