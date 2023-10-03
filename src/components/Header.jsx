e63cfcb8ac133e50f5b6ff3ea079ce185afca1fb
unknown <jorge.orozco@cetys.edu.mx>
2023-10-03 13:41:15 -0700

    Store init

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,9 +11,7 @@ const Header = () => {
                 <div className="collapse navbar-collapse" id="navbarColor02">
                 <ul className="navbar-nav me-auto">
                     <li className="nav-item">
-                    <Link className="nav-link active" to={'/login'}>Login
-                        <span className="visually-hidden">(current)</span>
-                    </Link>
+                    <Link className="nav-link" to={'/login'}>Login</Link>
                     </li>
                     <li className="nav-item">
                     <Link className="nav-link" to={'/register'}>Register</Link>
