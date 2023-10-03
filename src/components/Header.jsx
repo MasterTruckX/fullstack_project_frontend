ec66021228793323c033bb049ee3bc45beca2d0f
unknown <jorge.orozco@cetys.edu.mx>
2023-10-03 14:49:39 -0700

    Register page ready to use

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,18 @@
-import { Link } from 'react-router-dom'
+import { FaSignInAlt, FaSignOutAlt, FaUser } from 'react-icons/fa'
+import {Link, useNavigate} from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import { logout, reset } from '../features/auth/authSlice'
+
 const Header = () => {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const {user} = useSelector((state) => state.auth)
+
+  const onLogout = () => {
+    dispatch(logout())
+    dispatch(reset())
+    navigate('/login')
+  }
   return (
     <>
         <nav className="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
@@ -10,18 +23,24 @@ const Header = () => {
                 </button>
                 <div className="collapse navbar-collapse" id="navbarColor02">
                 <ul className="navbar-nav me-auto">
-                    <li className="nav-item">
-                    <Link className="nav-link" to={'/login'}>Login</Link>
-                    </li>
-                    <li className="nav-item">
-                    <Link className="nav-link" to={'/register'}>Register</Link>
-                    </li>
-                    {/* <li className="nav-item">
-                    <a className="nav-link" href="#">Pricing</a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="#">About</a>
-                    </li> */}
+                    {user? (
+                    <>
+                      <li>
+                        <button className='btn' onClick={onLogout}>
+                            <FaSignOutAlt /> Logout
+                        </button>
+                      </li>
+                    </>
+                    ) : (
+                    <>
+                      <li className="nav-item">
+                        <Link className="nav-link" to={'/login'}>Login</Link>
+                      </li>
+                      <li className="nav-item">
+                        <Link className="nav-link" to={'/register'}>Register</Link>
+                      </li>
+                    </>
+                    )}
                 </ul>
                 <form className="d-flex">
                     <input className="form-control me-sm-2" type="search" placeholder="Search" />
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-// import { useSelector, useDispatch } from 'react-redux'
-// import { useNavigate } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 import { FaUser } from 'react-icons/fa'
 import { toast } from 'react-toastify'
-// import { register, reset } from '../features/auth/authSlice'
-// import Spinner from '../components/Spinner'
+import { register, reset } from '../features/auth/authSlice'
+import Spinner from '../components/Spinner'
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -14,6 +14,25 @@ const Register = () => {
     password2: ''
   })
   const { name, email, password, password2 } = formData
+
+  const { user, isLoading, isError, isSuccess, message } = useSelector((state) => state.auth)
+
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+
+        if (isError) {
+            toast.error(message)
+        }
+
+        if (isSuccess || user) {
+            navigate('/login')
+        }
+
+        dispatch(reset())
+
+    }, [user, isError, isSuccess, message, navigate, dispatch])
   
   const onChange = (e) => {
     setFormData((prevState) => ({
@@ -32,9 +51,13 @@ const onSubmit = (e) => {
         const userData = {
             name, email, password
         }
-        
+        dispatch(register(userData))
     }
-}
+  }
+
+  if (isLoading) {
+    return <Spinner />
+  }
 
   return (
     <>
